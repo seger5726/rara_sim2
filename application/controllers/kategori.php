88f42516f4e9593eacf7848c5cac911d12b76c41
Marsyanda Rara Agustia <marsyandarara22@gmail.com>
2026-03-03 15:54:09 +0700

    3

---
+++ application/controllers/kategori.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH')OR exit('No direct script access allowed');
+
+class kategori extends CI_Controller{
+
+    public function __construct()
+    {
+        parent ::__construct();
+        $this->load->model('kategori_model');
+
+    }
+    public function index()
+    {
+        $data['kategori'] = $this->kategori_model->get_all();
+        $this->load->view('templates/header');
+        $this->load->view('templates/topbar');
+        $this->load->view('templates/sidebar');
+        $this->load->view('kategori/index', $data);
+        $this->load->view('templates/footer');
+
+    }
+    public function tambah()
+    {
+        $this->load->view('kategori/tambah');
+    }
+    public function simpan()
+    {
+        $data= [
+            'nama_kategori'=> $this->input->post('nama_kategori')
+        ];
+
+        $this->kategori_model->insert($data);
+        redirect('kategori');
+    }     
+}
